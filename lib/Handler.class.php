24b525434ef9017d996228d55b73fca9710f34ee
Роман Чаругин <roman-charugin@ya.ru>
2011-10-15 17:37:10 +0400

    Добавил в Handler.class.php полезный метод для получения списка переданных параметров

--- lib/Handler.class.php
+++ lib/Handler.class.php
@@ -17,7 +17,7 @@ class Handler {
 	 * @var mixed
 	 */
 	protected $access;
-	
+
 	/**
 	 * Геттер для dependency
 	 * 
@@ -26,7 +26,7 @@ class Handler {
 	public function GetDependencies() {
 		return $this->dependencies;
 	}
-	
+
 	/**
 	 * Геттер для access;
 	 * 
@@ -34,5 +34,22 @@ class Handler {
 	 */
 	public function GetAccess() {
 		return $this->access;
-	} 
+	}
+	
+	/**
+	 * Метод для получения "чистого" списка параметров запроса
+	 * @param mixed $beside Дополнительныо исключаемые элементы (либо строка для одного элемента, либо массив для нескольких)
+	 * @return array Ассоциативный массив параметров
+	 */
+	protected function GetParams($beside = null) {
+		$main_params = array('page', 'action');
+		if (!is_null($beside))
+			$main_params = array_merge($main_params, $beside);
+
+		$params = array();
+		foreach(array_merge($_GET, $_POST) AS $key => $val)
+			if (!in_array($key, $main_params))
+				$params[$key] = $val;
+		return $params;
+	}
 }
