dbbee6ef24c5af3d49a9e3f156dde47ce1fe0890
Роман Чаругин <roman-charugin@ya.ru>
2012-04-17 12:20:05 +0400

    Добавил метод для получение одного параметра и метод для получения сортированного (в порядке очереди параметров функции) массива параметров.

--- lib/Handler.class.php
+++ lib/Handler.class.php
@@ -44,7 +44,7 @@ class Handler {
 	protected function GetParams($beside = null) {
 		$main_params = array('page', 'action');
 		if (!is_null($beside))
-			$main_params = array_merge($main_params, $beside);
+			 $main_params = array_merge($main_params, is_array($beside) ? $beside : array($beside));
 
 		$params = array();
 		foreach(array_merge($_GET, $_POST) AS $key => $val)
@@ -53,4 +53,42 @@ class Handler {
 		return $params;
 	}
 
+	/**
+	 * Метод для получения конкретного параметра из запроса
+	 * @param string $name Имя параметра
+	 * @return string|null Значение параметра, либо null в случае если такого параметра не существует, либо он пуст
+	 */
+	protected function GetParam($name) {
+		if (!empty($_POST[$name]))
+			return $_POST[$name];
+
+		if (!empty($_GET[$name]))
+			return $_GET[$name];
+
+		return null;
+	}
+
+	/**
+	 * Получение списка параметров, отсортированного в соответствии с необходимой функцией или методом
+	 * @param string $function Имя функции или метода
+	 * @param string|object $class Имя класса или ссылка на экземпляр класса, соответсвующего метода
+	 * @return array|false Ассоциативный массив параметров, расположенных в нужном порядке, false - в случае ошибки
+	 */
+	public function GetOrderedParams($function, $class = null) {
+		try {
+			$ref = !is_null($class) ? new ReflectionMethod($class, $function) : new ReflectionFunction($function);
+
+			$res = array();
+
+			$method_params = $ref->getParameters();
+			foreach ($method_params as $mp) {
+				$user_param = $this->GetParam($mp->name);
+				$res[$mp->name] = !is_null($user_param) ? $user_param : ($mp->isDefaultValueAvailable() ? $mp->getDefaultValue() : null);
+			}
+
+			return $res;
+		} catch (ReflectionException $e) {
+			return false;
+		}
+	}
 }
